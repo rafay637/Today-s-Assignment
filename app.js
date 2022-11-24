0122f1941a1f8cad15bdd5de2ec8f6be2f98cb44
Syed Rafay <rafaysyed637@gmail.com>
2022-11-24 17:33:00 +0500

    done

---
+++ app.js
@@ -0,0 +1,69 @@
+// Start
+var b = prompt("Enter city name");
+a = b.toUpperCase();
+console.log(a);
+
+var a = prompt("Enter country name");
+b = a.toLowerCase();
+console.log(b);
+
+var c = prompt("Enter your favourite mobile phone model");
+var d = "length = 5cm";
+console.log(d);
+
+var b = prompt("Enter message.");
+lastletter = b.slice(-1);
+console.log(lastletter);
+
+var a = "Pakistani";
+var b = "n";
+var i = a.indexOf("n");
+console.log(i);
+
+var username = "a";
+var myname = "Syed Rafay";
+
+var message = "Hello World";
+alert(message);
+
+var stbio1 = "Umar";
+var stbio2 = "15 years old";
+var stbio3 = "Certified Mobile Application Development";
+alert(stbio1);
+alert(stbio2);
+alert(stbio3);
+
+var food = "pizza";
+alert("pizza" + "\n" + "pizz" + "\n" + "piz" + "\n" + "pi" + "\n" + "p");
+
+var email = "My email address is rafaysyed637@gmail.com";
+alert(email);
+
+var book = "I am trying to learn from the book A Smarter Way To Learn JavaScript";
+alert(book);
+
+var script = "Yah! I can write HTML content through JavaScript";
+alert(script);
+
+var design = " SIOS ";
+alert(design);
+
+var no1 = 3;
+var no2 = 5;
+var sumOfno = 8;
+alert(sumOfno);
+
+var a = 600;
+var b = 5;
+var c = b * a;
+alert("Total cost to buy 5 tickets to a movie is " + c + "PKR");
+
+var d = 10;
+var e = 104.80;
+var f = d * e;
+var g = 25;
+var h = 28;
+var i = g * h;
+var j = f + i;
+alert("Total Currency in PKR: " + j)
+// End
